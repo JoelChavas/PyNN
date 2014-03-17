5bafc0de66a7c53ba0bc77e7c03ae50cd68c6a08
JoelChavas <joel.chavas@unic.cnrs-gif.fr>
2014-03-17 16:04:28 +0000

    connect() is running

--- src/hardware/brainscales/populations.py
+++ src/hardware/brainscales/populations.py
@@ -4,6 +4,7 @@ from pyNN.standardmodels import StandardCellType
 from pyNN.parameters import ParameterSpace, simplify
 from . import simulator
 from .recording import Recorder
+import pyNN.hardware.brainscales
 
 
 
@@ -55,35 +56,23 @@ class Population(common.Population):
         __doc__ = common.Population.__doc__
         common.Population.__init__(self, size, cellclass, cellparams,
                                    structure, initial_values, label)
-        simulator.initializer.register(self)
 
     def _create_cells(self):
         # this method should never be called more than once
         # perhaps should check for that
-        self.first_id = simulator.state.gid_counter
-        self.last_id = simulator.state.gid_counter + self.size - 1
-        self.all_cells = numpy.array([id for id in range(self.first_id, self.last_id+1)], 
-                                     simulator.ID)
-        def is_local(id):
-            return (id % simulator.state.num_processes) == simulator.state.mpi_rank
-        self._mask_local = is_local(self.all_cells)
+        buf = pyNN.hardware.brainscales._create(self.celltype, None, self.size, cell_tag=self.label)
+        if type(buf) != type([]): buf = [buf]
+        self.all_cells = buf
+        for c in self.all_cells:     # set parent
+            c.parent = self
+        self._mask_local = numpy.ones((self.size,), bool) # all cells are local. This doesn't seem very efficient.
         
-        if isinstance(self.celltype, StandardCellType):
-            parameter_space = self.celltype.native_parameters
-        else:
-            parameter_space = self.celltype.parameter_space
-        parameter_space.shape = (self.size,)
-        parameter_space.evaluate(mask=self._mask_local, simplify=False)
-        self._parameters = parameter_space.as_dict()
         
-        for i, (id, is_local, params) in enumerate(zip(self.all_cells, self._mask_local, parameter_space)):
-            self.all_cells[i] = simulator.ID(id)
-            self.all_cells[i].parent = self
-            if is_local:
-                if hasattr(self.celltype, "extra_parameters"):
-                    params.update(self.celltype.extra_parameters)
-                self.all_cells[i]._build_cell(self.celltype.model, params)
-        simulator.state.gid_counter += self.size
+        self.all_cells = numpy.array( [ c for c in self.all_cells], pyNN.hardware.brainscales.ID  )
+        
+        self.first_id = int(self.all_cells[0])
+        self.last_id = int(self.all_cells[-1])        
+        simulator.state.id_counter += self.size
 
     def _set_initial_value_array(self, variable, initial_values):
         pass
