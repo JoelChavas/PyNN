9667f8c5ea44f4e8608e4e4902374fdb061e979f
JoelChavas <joel.chavas@unic.cnrs-gif.fr>
2014-06-16 15:30:55 +0000

    correct separation between pynn and symap2ic

--- src/hardware/aux.py
+++ src/hardware/aux.py
@@ -5,6 +5,7 @@ def get_installation(target="brainscales"):
         symap2ic_path = os.environ['SYMAP2IC_PATH']
         if os.environ.has_key('PYNN_HW_PATH'): target_path = os.path.join(os.environ['PYNN_HW_PATH'],target)
         else: target_path = os.path.join(symap2ic_path,"components/pynnhw/src/hardware", target)
+        hardware_path=os.path.join(symap2ic_path,"components/pynnhw/src/hardware")
     else:
         raise Exception("Variable SYMAP2IC_PATH not set!")
-    return symap2ic_path, target_path
+    return symap2ic_path, hardware_path, target_path
--- src/hardware/brainscales.py
+++ src/hardware/brainscales.py
@@ -1,6 +1,11 @@
-import os
-import aux
+from os import environ, path
+from sys import path as syspath
 
-if __name__ == 'pyNN.hardware.brainscales':
-    symap2ic_path, target_path = aux.get_installation('brainscales')
-    execfile(os.path.join(target_path,'__init__.py'))
+#if __name__ == 'pyNN.hardware.brainscales':
+if environ.has_key('SYMAP2IC_PATH'):
+    symap2ic_path = environ['SYMAP2IC_PATH']
+    hardware_path=path.join(symap2ic_path,"components/pynnhw/src")
+    syspath.insert(0,hardware_path)
+    from hardware.brainscales import *
+else:
+    raise Exception("Variable SYMAP2IC_PATH not set!")
