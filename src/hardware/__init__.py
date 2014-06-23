a3a15772f09b3643484830a39a789b521df4e3d1
JoelChavas <joel.chavas@unic.cnrs-gif.fr>
2014-06-23 14:49:40 +0000

    solving in a proper wau the inclusion of brainscales inside the pyNN structure

--- src/hardware/__init__.py
+++ src/hardware/__init__.py
@@ -1,1 +1,10 @@
-__all__ = ["brainscales"]
+from os import environ, path
+
+#if __name__ == 'pyNN.hardware.brainscales':
+if environ.has_key('SYMAP2IC_PATH'):
+    symap2ic_path = environ['SYMAP2IC_PATH']
+    hardware_path=path.join(symap2ic_path,"components/pynnhw/src")
+    __path__.append(path.join(hardware_path,"hardware"))
+    import brainscales
+else:
+    raise Exception("Variable SYMAP2IC_PATH not set!")
